ea19903983bc39fe96743a947f919d6c8cfa7b43
ZylockTheWizard <cml0011@aol.com>
2025-02-18 12:47:44 -0600

    fix: loger error fix

--- src/logger.ts
+++ src/logger.ts
@@ -12,7 +12,7 @@ export class Logger
             fs.mkdirSync(this.logFolder)
         }
         process.on('uncaughtExceptionMonitor', err => {
-            this.error(JSON.stringify(err, Object.getOwnPropertyNames(err)))
+            this.error('uncaughtExcpetion', err)
         })
     }
 
@@ -20,6 +20,11 @@ export class Logger
     {
         const output = typeof message === 'object' ? JSON.stringify(message) : message
         return new Date().toISOString() + ' - ' + output
+    }
+
+    private static standardizeError(err: Error)
+    {
+        return this.standardize(JSON.stringify(err, Object.getOwnPropertyNames(err)))
     }
 
     private static writeLine(file: string, line: string)
@@ -34,11 +39,17 @@ export class Logger
         this.writeLine(this.logFile, output)
     }
 
-    public static error(message: string | object)
+    public static error(message: string | object, err?: Error)
     {
         const output = this.standardize(message)
         console.error(output)
         this.writeLine(this.logFile, output)
         this.writeLine(this.errorFile, output)
+        if(err) {
+            const output2 = this.standardizeError(err)
+            console.error(output2)
+            this.writeLine(this.logFile, output2)
+            this.writeLine(this.errorFile, output2)
+        }
     }
 }
