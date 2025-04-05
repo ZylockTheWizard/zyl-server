e93229d32a6004213785c46d1f3e0af5c8cca82b
ZylockTheWizard <cml0011@aol.com>
2025-04-05 14:43:52 -0500

    chore: query updates

--- src/globals.ts
+++ src/globals.ts
@@ -6,50 +6,48 @@ export type ServerDefault = Server<DefaultEventsMap, DefaultEventsMap, DefaultEv
 export const areEqual = (a: string | undefined, b: string | undefined) =>
     a?.toLowerCase() === b?.toLowerCase()
 
-export type KeyString = { [key: string]: string }
-export type KeyRaw = { [key: string]: string | number }
-
-const keyVal = (obj: object) => ({ key: Object.keys(obj)[0], val: Object.values(obj)[0] })
-
-export const str = (val: string) => `'${val}'`
-export const keyStr = (obj: KeyString) => {
-    const { key, val } = keyVal(obj)
-    return `${key} = ${str(val)}`
-}
-export const keyStrObj = (obj: KeyString) => {
-    const { key, val } = keyVal(obj)
-    return { key, val: str(val) }
+export type QueryObject = {
+    [key: string]: string | number | boolean
+    string?: boolean
 }
 
-export const raw = (val: string | number) => val.toString()
-export const keyRaw = (obj: KeyRaw) => {
+const keyVal = (obj: QueryObject) => ({ key: Object.keys(obj)[0], val: Object.values(obj)[0] })
+const raw = (val: string | number | boolean) => (!val ? 'NULL' : val.toString())
+const str = (val: string | number | boolean) => (!val ? 'NULL' : `'${val}'`)
+
+const dbObject = (obj: QueryObject) => {
     const { key, val } = keyVal(obj)
-    return `${key} = ${raw(val)}`
-}
-export const keyRawObj = (obj: KeyRaw) => {
-    const { key, val } = keyVal(obj)
-    return { key, val: raw(val) }
+    return { key, val: obj.string ? str(val) : raw(val) }
 }
 
-export const selectClause = (select: string[]) => {
+const equalString = (obj: QueryObject) => {
+    const { key, val } = dbObject(obj)
+    return `${key} = ${val}`
+}
+
+const selectClause = (select: string[]) => {
     return 'SELECT ' + (select?.length > 0 ? select.join() : '*')
 }
 
-export const fromClause = (table: string) => `FROM ${table}`
+const fromClause = (table: string) => `FROM ${table}`
 
-export const whereClause = (where: string) => {
-    return where ? 'WHERE ' + where : undefined
+const whereClause = (where: QueryObject) => {
+    return where ? 'WHERE ' + equalString(where) : undefined
 }
 
-export const buildSelectQuery = (table: string, props?: { select?: string[]; where?: string }) => {
-    const clauses = [selectClause(props?.select), fromClause(table), whereClause(props?.where)]
-    return clauses.join(' ')
+export const buildSelectQuery = (
+    table: string,
+    props?: { select?: string[]; where?: QueryObject },
+) => {
+    return [selectClause(props?.select), fromClause(table), whereClause(props?.where)].join(' ')
 }
 
-export const buildUpdateQuery = (table: string, updates: string[], where: string) => {
-    return `UPDATE ${table} SET ${updates.join()} WHERE ${where}`
+export const buildUpdateQuery = (table: string, updates: QueryObject[], where: QueryObject) => {
+    const updateString = updates.map((u) => equalString(u)).join()
+    return `UPDATE ${table} SET ${updateString} ${whereClause(where)}`
 }
 
-export const buildInsertQuery = (table: string, values: { key: string; val: any }[]) => {
-    return `INSERT INTO ${table} (${values.map((v) => v.key).join()}) VALUES (${values.map((v) => v.val).join()})`
+export const buildInsertQuery = (table: string, values: QueryObject[]) => {
+    const dbObjects = values.map((v) => dbObject(v))
+    return `INSERT INTO ${table} (${dbObjects.map((v) => v.key).join()}) VALUES (${dbObjects.map((v) => v.val).join()})`
 }
--- src/queries.ts
+++ src/queries.ts
@@ -1,51 +1,46 @@
-import {
-    buildInsertQuery,
-    buildSelectQuery,
-    buildUpdateQuery,
-    keyRaw,
-    keyRawObj,
-    keyStr,
-    keyStrObj,
-} from './globals'
+import { buildInsertQuery, buildSelectQuery, buildUpdateQuery, QueryObject } from './globals'
 
 export const userQuery = (id?: string) => {
-    const where = id ? keyStr({ id }) : undefined
+    const where: QueryObject = id ? { id, string: true } : undefined
     return buildSelectQuery('users', { where })
 }
 
 export const passwordResetQuery = (id: string, password: string) => {
-    const updates = [keyStr({ password }), keyRaw({ passwordReset: 0 })]
-    return buildUpdateQuery('users', updates, keyStr({ id }))
+    const updates: QueryObject[] = [{ password, string: true }, { passwordReset: 0 }]
+    return buildUpdateQuery('users', updates, { id, string: true })
 }
 
 export const createUserQuery = (id: string) => {
-    const values = [
-        keyStrObj({ id }),
-        keyRawObj({ password: 'UUID()' }),
-        keyRawObj({ passwordReset: 1 }),
+    const values: QueryObject[] = [
+        { id, string: true },
+        { password: 'UUID()' },
+        { passwordReset: 1 },
     ]
     return buildInsertQuery('users', values)
 }
 
 export const sceneQuery = (select?: string[], where?: { id?: string; name?: string }) => {
-    let whereString = undefined
+    let whereObject: QueryObject = undefined
     if (where) {
         const { id, name } = where
-        if (id) whereString = keyStr({ id })
-        else if (name) whereString = keyStr({ name })
+        if (id) whereObject = { id, string: true }
+        else if (name) whereObject = { name, string: true }
     }
-    return buildSelectQuery('scenes', { select, where: whereString })
+    return buildSelectQuery('scenes', { select, where: whereObject })
 }
 
 export const createSceneQuery = (name: string, data: string) => {
-    const values = [keyStrObj({ name }), keyStrObj({ data })]
+    const values: QueryObject[] = [
+        { name, string: true },
+        { data, string: true },
+    ]
     return buildInsertQuery('scenes', values)
 }
 
 export const setCurrentSceneQuery = (sceneId: string, user: string) => {
-    return buildUpdateQuery('users', [keyRaw({ sceneId })], keyStr({ id: user }))
+    return buildUpdateQuery('users', [{ sceneId }], { id: user, string: true })
 }
 
 export const updateSceneDataQuery = (id: string, data: string) => {
-    return buildUpdateQuery('scenes', [keyStr({ data })], keyStr({ id }))
+    return buildUpdateQuery('scenes', [{ data, string: true }], { id, string: true })
 }
